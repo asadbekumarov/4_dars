e222263678d9376ff5acb1183589645f619db412
asadbekumarov <asadbekumarov922@gmail.com>
2024-08-28 19:09:47 +0500

    lesson

---
+++ js/app.js
@@ -0,0 +1,181 @@
+// 1 - assignment;
+// {
+//   let a = +prompt(`buton son kiriting`);
+//   if (a > 0) {
+//     a++;
+//     console.log(`${a} musbat`);
+//   } else {
+//     console.log(`${a} manfiy`);
+//   }
+// }
+
+// 2 - assignment;
+// {
+//   let a = +prompt(`buton son kiriting`);
+//   if (a > 0) {
+//     a++;
+//     console.log(`${a} musbat`);
+//   } else {
+//     a--;
+//     a--;
+//     console.log(`${a} manfiy`);
+//   }
+// }
+
+// // 3 - assignment;
+// {
+//   let a = +prompt(`buton son kiriting`);
+//   if (a > 0) {
+//     a++;
+//     console.log(`${a} musbat`);
+//   } else if (a === 0) {
+//     a = a + 10;
+//     console.log(`${a}`);
+//   } else {
+//     a--;
+//     a--;
+//     console.log(`${a} manfiy`);
+//   }
+// }
+
+// // 4 - assignment;
+// {
+//   let a = +prompt(`buton son kiriting`);
+//   let b = +prompt(`buton son kiriting`);
+//   let c = +prompt(`buton son kiriting`);
+//   let musbat = 0;
+//   let manfiy = 0;
+//   if (a > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   if (b > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   if (c > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   console.log(`musabt ${musbat}`);
+// }
+
+// 5 - assignment;
+// {
+//   {
+//     let a = +prompt(`buton son kiriting`);
+//     let b = +prompt(`buton son kiriting`);
+//     let c = +prompt(`buton son kiriting`);
+//     let musbat = 0;
+//     let manfiy = 0;
+//     if (a > 0) {
+//       musbat++;
+//     } else {
+//       manfiy++;
+//     }
+//     if (b > 0) {
+//       musbat++;
+//     } else {
+//       manfiy++;
+//     }
+//     if (c > 0) {
+//       musbat++;
+//     } else {
+//       manfiy++;
+//     }
+//     console.log(` manfiy ${manfiy} musabt ${musbat}`);
+//   }
+// }
+
+// 6 - assignment;
+// {
+//   let a = +prompt(`son kiriting`);
+//   let b = +prompt(`son kiriting`);
+//   if (a > b) {
+//     console.log(`${a}`);
+//   } else {
+//     console.log(`${b}`);
+//   }
+// }
+
+// // 7 - assignment;
+// {
+//   let a = +prompt(`Birinchi sonni kiriting`);
+//   let b = +prompt(`Ikkinchi sonni kiriting`);
+
+//   if (a < b) {
+//     console.log(`kichik son birinchi kiritilgan`);
+//   } else if (b < a) {
+//     console.log(`kichik son ikkinchi kiritilgan`);
+//   } else {
+//     console.log(`ikkala son ham teng`);
+//   }
+// }
+
+// // 8 - assignment;
+// {
+//   let a = +prompt(`son kiriting`);
+//   let b = +prompt(`son kiriting`);
+//   if (a > b) {
+//     console.log(`${a} katta son ${b} kichik son`);
+//   } else {
+//     console.log(`${b} katta son ${a} kichik son`);
+//   }
+// }
+
+// // 9 - assignment;
+// {
+//   {
+//     let a = +prompt(`son kiriting`);
+//     let b = +prompt(`son kiriting`);
+//     if (a > b) {
+//       console.log(`${b} kichik son ${a} katta son`);
+//     } else {
+//       console.log(`${a} kichik son ${b} katta son`);
+//     }
+//     c = a + b;
+//     console.log(`${c}`);
+//   }
+// }
+
+// 10 - assignment;
+// let a = +prompt(`son kiriting`);
+// let b = +prompt(`son kiriting`);
+
+// if (a !== b) {
+//   c = a + b;
+//   console.log(`${c}`);
+// } else {
+//   a = 0;
+//   b = 0;
+//   console.log(`${a}  ${b}`);
+// }
+
+// SWITCH CASE
+
+// let pul = prompt(`pullingiz qanchaligini kiritng`);
+// switch (pul) {
+//   case `90`:
+//     console.log(`gentraga yetadi`);
+//     break;
+//   case `80`:
+//     console.log(`cobaltga yetadi`);
+//     break;
+//   case `70`:
+//     console.log(`nexia 3ga yetadi`);
+//     break;
+//   case `60`:
+//     console.log(`sparkga yetadi`);
+//     break;
+//   case `50`:
+//     console.log(`damasga yetadi`);
+//     break;
+//   case `40`:
+//     console.log(`matizga yetadi`);
+//     break;
+//   default:
+//     console.log(`hali ishlashingiz kerak😂`);
+// }
